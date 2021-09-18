c9bf78679a327c1c41737a01127cb0655a628a11
andregoro123456@gmail.com <andregoro123456@gmail.com>
2021-09-18 02:39:49 -0300

    atualização

--- blog/app/Http/Controllers/ProductController.php
+++ blog/app/Http/Controllers/ProductController.php
@@ -2,22 +2,204 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    //
+    protected  $request,$user;
+    public function __construct(Request $request,User $user)
+    {
+        $this->user=$user;
+        $this->request=$request;
+        $this->middleware('auth')->except([
+            'index','show'
+        ]);
+    }
+    // public function store(Request $request){
+    //     //$request =new
+    // }
     public function index()
     {
-        $teste="<h1>Andre</h1>";
-        $products=['Products 01','Products 02','Product 03'];
+        $this->request;
+       // $products=['Product 01','Product 02','Product 03'];
+        //return $products;
+       return view('teste');//'Listagem';
+    }
+    public function show($id)
+    {
+        return "Exibindo o id {$id}";
+    }
 
-        return $teste;
+    public function create()
+    {
+        return "Exibindo o form";
     }
-    public function show($id){
-        return "Exibindo {$id}";
+
+    public function edit($id)
+    {
+        return "Editando o form";
     }
-    public function create(){
-        return "Exibindo crud";
-    }
-}
+//////////////////////////////////////////////////////////
+//     protected $request;
+//     private $repository;
+
+//     public function __construct(Request $request, Product $product)
+//     {
+//         $this->request = $request;
+//         $this->repository = $product;
+
+//         $this->middleware('auth');
+//         $this->middleware('auth')->only([
+//             'create', 'store'
+//         ]);
+//         $this->middleware('auth')->except([
+//             'index', 'show'
+//         ]);
+//     }
+
+
+//     /**
+//      * Display a listing of the resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function index()
+//     {
+//         $products = Product::latest()->paginate();
+
+//         return view('admin.pages.products.index', [
+//             'products' => $products,
+//         ]);
+//     }
+
+//     /**
+//      * Show the form for creating a new resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function create()
+//     {
+//         return view('admin.pages.products.create');
+//     }
+
+//     /**
+//      * Store a newly created resource in storage.
+//      *
+//      * @param  \App\Http\Requests\StoreUpdateProductRequest  $request
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function store(StoreUpdateProductRequest $request)
+//     {
+//         $data = $request->only('name', 'description', 'price');
+
+//         if ($request->hasFile('image') && $request->image->isValid()) {
+//             $imagePath = $request->image->store('products');
+
+//             $data['image'] = $imagePath;
+//         }
+
+//         $this->repository->create($data);
+
+//         return redirect()->route('products.index');
+//     }
+
+//     /**
+//      * Display the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function show($id)
+//     {
+//         // $product = Product::where('id', $id)->first();
+//         if (!$product = $this->repository->find($id))
+//             return redirect()->back();
+
+//         return view('admin.pages.products.show', [
+//             'product' => $product
+//         ]);
+//     }
+
+//     /**
+//      * Show the form for editing the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function edit($id)
+//     {
+//         if (!$product = $this->repository->find($id))
+//             return redirect()->back();
+
+//         return view('admin.pages.products.edit', compact('product'));
+//     }
+
+//     /**
+//      * Update the specified resource in storage.
+//      *
+//      * @param  \App\Http\Requests\StoreUpdateProductRequest  $request
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     // public function update(StoreUpdateProductRequest $request, $id)
+//     // {
+//     //     if (!$product = $this->repository->find($id))
+//     //         return redirect()->back();
+
+//     //     $data = $request->all();
+
+//     //     if ($request->hasFile('image') && $request->image->isValid()) {
+
+//     //         if ($product->image && Storage::exists($product->image)) {
+//     //             Storage::delete($product->image);
+//     //         }
+
+//     //         $imagePath = $request->image->store('products');
+//     //         $data['image'] = $imagePath;
+//     //     }
+
+//     //     $product->update($data);
+
+//     //     return redirect()->route('products.index');
+//     // }
+
+//     /**
+//      * Remove the specified resource from storage.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function destroy($id)
+//     {
+//         $product = $this->repository->where('id', $id)->first();
+//         if (!$product)
+//             return redirect()->back();
+
+//         if ($product->image && Storage::exists($product->image)) {
+//             Storage::delete($product->image);
+//         }
+
+//         $product->delete();
+
+//         return redirect()->route('products.index');
+//     }
+
+//     /**
+//      * Search Products
+//      */
+//     public function search(Request $request)
+//     {
+//         $filters = $request->except('_token');
+
+//         $products = $this->repository->search($request->filter);
+
+//         return view('admin.pages.products.index', [
+//             'products' => $products,
+//             'filters' => $filters,
+//         ]);
+//     }
+ }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,90 +16,100 @@ Route::get('products/create','ProductController@create');
 Route::get('products/{id}','ProductController@show')->name('products.index');
 Route::get('products','ProductController@index')->name('products.show');
 */
-Route::resource('products','ProductController');
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::resource('products','ProductController');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('products','ProductController');//->middleare('auth');
+// Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+// Route::put('products/{id}', 'ProductController@update')->name('products.update');
+// Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+// Route::get('products/create', 'ProductController@create')->name('products.create');
+// Route::get('products/{id}', 'ProductController@show')->name('products.show');
+// Route::get('products', 'ProductController@index')->name('products.index');
+// Route::post('products', 'ProductController@store')->name('products.store');
 
 Route::get('/login', function () {
     return 'login';
 })->name('login');
 
-Route::middleware(['auth'])->group(function()
-{   
-    Route::prefix('admin')->group(function()
-    {
-        
-    });
-    Route::get('/admin/dashboard', function () {
-        return 'Home Admin';
-    });
-    
-    Route::get('/admin/financeiro', function () {
-        return 'Financeiro Admin';
-    });
-    
-    Route::get('/admin/produto', function () {
-        return 'Produto Admin';
-    });
-});
+// Route::middleware(['auth'])->group(function()
+// {
+//     Route::prefix('admin')->group(function()
+//     {
 
+//     });
+//     Route::get('/admin/dashboard', function () {
+//         return 'Home Admin';
+//     });
 
+//     Route::get('/admin/financeiro', function () {
+//         return 'Financeiro Admin';
+//     });
 
-Route::get('redirect3', function () {
-    return redirect()->route('url.name');
-});
-
-// route('url.name');
-
-Route::get('/nome-url', function () {
-    return 'Hey hey hey';
-})->name('url.name');
-
-Route::view('/view', 'welcome', ['id' => 'teste']);
-
-Route::redirect('/redirect1', '/redirect2');
-
-// Route::get('redirect1', function () {
-//     return redirect('/redirect2');
+//     Route::get('/admin/produto', function () {
+//         return 'Produto Admin';
+//     });
 // });
 
-Route::get('redirect2', function () {
-    return 'Redirect 02';
-});
 
-Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
-    return "Produto(s) {$idProduct}";
-});
 
-Route::get('/categoria/{flag}/posts', function ($flag) {
-    return "Posts da categoria: {$flag}";
-});
+// Route::get('redirect3', function () {
+//     return redirect()->route('url.name');
+// });
 
-Route::get('/categorias/{flag}', function ($prm1) {
-    return "Produtos da categoria: {$prm1}";
-});
+// // route('url.name');
 
-Route::match(['post', 'get'], '/match', function () {
-    return 'match';
-});
+// Route::get('/nome-url', function () {
+//     return 'Hey hey hey';
+// })->name('url.name');
 
-Route::any('/any', function () {
-    return 'Any';
-});
+// Route::view('/view', 'welcome', ['id' => 'teste']);
 
-Route::post('/register', function () {
-    return 's';
-});
+// Route::redirect('/redirect1', '/redirect2');
 
-Route::get('/empresa', function () {
-    return view('site.contact');
-});
+// // Route::get('redirect1', function () {
+// //     return redirect('/redirect2');
+// // });
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+// Route::get('redirect2', function () {
+//     return 'Redirect 02';
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+//     return "Produto(s) {$idProduct}";
+// });
+
+// Route::get('/categoria/{flag}/posts', function ($flag) {
+//     return "Posts da categoria: {$flag}";
+// });
+
+// Route::get('/categorias/{flag}', function ($prm1) {
+//     return "Produtos da categoria: {$prm1}";
+// });
+
+// Route::match(['post', 'get'], '/match', function () {
+//     return 'match';
+// });
+
+// Route::any('/any', function () {
+//     return 'Any';
+// });
+
+// Route::post('/register', function () {
+//     return 's';
+// });
+
+// Route::get('/empresa', function () {
+//     return view('site.contact');
+// });
+
+// Route::get('/contato', function () {
+//     return 'Contato';
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
